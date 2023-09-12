305b6bb7a3a7c7558e94283f9b9bf9da1f710c98
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-09-12 19:43:13 +0300

    command argument

--- Shell.c
+++ Shell.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main - Entry point
  *
@@ -9,14 +11,19 @@
 int main(void)
 {
 	char prompat[] = "shell $ ";
-	char *linestr = NULL;
+	char *linestr = NULL, *linestrCopy = NULL;
 	size_t n = 0;
 	int num_char;
+	char delim = ' ';
+
 
 	while (1)
 	{
 		printf("%s", prompat);
 		num_char = getline(&linestr, &n, stdin);
+		linestrCopy = malloc(sizeof(char) * num_char);
+		strcpy(linestrCopy, linestr);
+		printf(linestrCopy);
 		if (num_char == -1)
 		{
 			break;
