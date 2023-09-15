8c7c45d70bac989af40db8c549f402129948b271
AmiraWalid1 <amirawaleed012@gmail.com>
2023-09-15 18:22:02 +0300

    spilt line

--- Shell.c
+++ Shell.c
@@ -11,39 +11,52 @@
 int main(void)
 {
 	char prompat[] = "shell $ ";
-	char *linestr = NULL, *linestrCopy = NULL, *word, *list_of_words;
+	char *linestr = NULL, *linestrCopy = NULL, *word, **list_of_words;
 	size_t n = 0;
 	int num_char, num_words = 0, i;
 	const char *delim = " ";
-
 
 	while (1)
 	{
 		printf("%s", prompat);
 		num_char = getline(&linestr, &n, stdin);
+		if (num_char == -1)
+		{
+			return (-1);
+		}
 		linestrCopy = malloc(sizeof(char) * num_char);
+		if (linestrCopy == NULL)
+		{
+			perror("Memory allocated error");
+			return (-1);
+		}
 		strcpy(linestrCopy, linestr);
 		word = strtok(linestrCopy, delim);
-		while(word != NULL)
+		while (word != NULL)
 		{
 			num_words++;
 			word = strtok(NULL, delim);
 		}
 		num_words++;
 		list_of_words = malloc(sizeof(char *) * num_words);
+		if (list_of_words == NULL)
+		{
+			perror("Memory allocated error");
+			return (-1);
+		}
 		word = strtok(linestr, delim);
 		for (i = 0; word != 0; i++)
 		{
 			list_of_words[i] = malloc(sizeof(char) * strlen(word));
+			if (list_of_words[i] == NULL)
+			{
+				perror("Memory allocated error");
+				return (-1);
+			}
 			strcpy(list_of_words[i], word);
 			word = strtok(NULL, delim);
 		}
 		list_of_words[i] = NULL;
-		if (num_char == -1)
-		{
-			break;
-		}
-		free(linestr);
 	}
 	return (0);
 }
