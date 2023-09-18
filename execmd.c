f9082cd09c20aa753410815f972bf1a78e08c287
AmiraWalid1 <amirawaleed012@gmail.com>
2023-09-18 15:38:22 +0300

    exit

--- execmd.c
+++ execmd.c
@@ -8,33 +8,42 @@
 void execmd(char **argv)
 {
 	char *command = NULL, *actual_pathname = NULL;
-	pid_t id = fork();
+	pid_t id;
 
 	if (argv)
 	{
-		if (strcmp(argv[0], "exit") == 0)
-		{
-			if (id > 0)
-				printf("exits the shell\n");
-			kill(id, SIGTERM);
-			exit(0);
-		}
 		command = argv[0];
+		if (strcmp(command, "exit") == 0)
+		{
+			exit(EXIT_SUCCESS);
+		}
 		actual_pathname = get_location(command);
-	}
-	if (id == 0)
-	{
-		if (execve(actual_pathname, argv, NULL) == -1)
+		if (actual_pathname)
+		{
+			id = fork();
+			if (id == 0)
+			{
+				if (execve(actual_pathname, argv, NULL) == -1)
+				{
+					perror("./shell");
+				}
+			}
+			else if (id < 0)
+			{
+				perror("./shell");
+			}
+			else
+			{
+				wait(NULL);
+			}
+		}
+		else
 		{
 			perror("./shell");
 		}
 	}
-	else if (id < 0)
-	{
-		perror("./shell");
-	}
 	else
 	{
-		wait(NULL);
+		perror("./shell");
 	}
 }
