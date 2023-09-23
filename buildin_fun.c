15d9fb9a7cbf843bed33a28b6c25da08100fd94f
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-09-23 04:44:37 +0300

    betty

--- buildin_fun.c
+++ buildin_fun.c
@@ -48,14 +48,17 @@ void cd_fun(char **argv, char *linestr)
 	(void)linestr;
 	if (argv)
 	{
-		if (argv[1] == NULL || strcmp(argv[1], "~") == 0 && getenv("HOME") != NULL)
+		if (argv[1] == NULL || strcmp(argv[1], "~") == 0)
 		{
-			if (chdir(getenv("HOME")) != 0)
+			if (getenv("HOME") != NULL)
 			{
-				fprintf(stderr, "./hsh: %d: %s: ", command_num(), argv[0]);
-				fprintf(stderr, "can't cd to %s\n", "HOME");
+				if (chdir(getenv("HOME")) != 0)
+				{
+					fprintf(stderr, "./hsh: %d: %s: ", command_num(), argv[0]);
+					fprintf(stderr, "can't cd to %s\n", "HOME");
+				}
+				setenv("PWD", getenv("HOME"), 1);
 			}
-			setenv("PWD", getenv("HOME"), 1);
 		}
 		else if (strcmp(argv[1], "-") == 0)
 		{
